bfc86eb67e74e6eaa478d28bc1ab04bc833d897f
DanyFahrurrozi <danyfahrurrozi69@gmail.com>
2022-01-18 15:31:21 -0800

    Ujian Akhir Semester

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DivisionComponent } from './division/division.component';
+import { AddDataComponent } from './add-data/add-data.component';
+import { EditDataComponent } from './edit-data/edit-data.component';
+import { DeleteDataComponent } from './delete-data/delete-data.component';
 
 const routes: Routes = [
   {path:"dashboard",component:DashboardComponent},
-  {path:"divisi",component:DivisionComponent}
+  {path:"divisi",component:DivisionComponent},
+  {path:"adddata",component:AddDataComponent},
+  {path:"editdata",component:EditDataComponent},
+  {path:"deletedata",component:DeleteDataComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { AppMenuComponent } from './app-menu/app-menu.component';
 import { DivisionComponent } from './division/division.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AddDataComponent } from './add-data/add-data.component';
+import { EditDataComponent } from './edit-data/edit-data.component';
+import { DeleteDataComponent } from './delete-data/delete-data.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,10 @@ import { AppFooterComponent } from './app-footer/app-footer.component';
     AppMenuComponent,
     DivisionComponent,
     DashboardComponent,
-    AppFooterComponent
+    AppFooterComponent,
+    AddDataComponent,
+    EditDataComponent,
+    DeleteDataComponent,
   ],
   imports: [
     BrowserModule,
